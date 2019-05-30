f7cbd7a7acb6285423b02fdbf53cc21712db2db4
cesarsld <jaimes.cesar88@gmail.com>
2019-05-30 15:12:16 +0100

    removed use of .env file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-﻿import * as dotenv from "dotenv";
+﻿//import * as dotenv from "dotenv";
 import app from "./app";
-dotenv.config();
+//dotenv.config();
 var port = process.env.PORT || 1337;
 require ('./mongo/mongo');
 require('./controllers/TourneyController');
 require('./controllers/UserController');
+console.log('test')
 app.listen(port, () => {
     console.log('Express server listening on port ' + port);
 })
